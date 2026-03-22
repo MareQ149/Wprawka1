1c169cdcf51c7b839fd4f5f27d5a39f091f99d77
Mariusz <mariusz.kolendo2004@gmail.com>
2026-03-22 13:32:08 +0100

    Dodano character.cs

---
+++ Wprawka1/Models/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wprawka1.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        
+        public string Name { get; set; } = null!;
+
+        
+        public string Description { get; set; }
+
+        
+        public string Class { get; set; }
+
+        public int Level { get; set; }
+    }
+}
