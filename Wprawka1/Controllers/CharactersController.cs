ddc3b12b345a0612ec8bb847959e02fb082d6606
Mariusz <mariusz.kolendo2004@gmail.com>
2026-03-28 11:53:12 +0100

    Dodano CRUD dal characters i guild - wprawka 2

---
+++ Wprawka1/Controllers/CharactersController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Wprawka1.Controllers
+{
+    public class CharactersController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CharactersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Characters
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Characters.Include(c => c.Guild);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Characters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters
+                .Include(c => c.Guild)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return View(character);
+        }
+
+        // GET: Characters/Create
+        public IActionResult Create()
+        {
+            ViewData["GuildId"] = new SelectList(_context.Guilds, "Id", "Name");
+            return View();
+        }
+
+        // POST: Characters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,Class,Level,GuildId")] Character character)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(character);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GuildId"] = new SelectList(_context.Guilds, "Id", "Name", character.GuildId);
+            return View(character);
+        }
+
+        // GET: Characters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters.FindAsync(id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+            ViewData["GuildId"] = new SelectList(_context.Guilds, "Id", "Name", character.GuildId);
+            return View(character);
+        }
+
+        // POST: Characters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,Class,Level,GuildId")] Character character)
+        {
+            if (id != character.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(character);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CharacterExists(character.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GuildId"] = new SelectList(_context.Guilds, "Id", "Name", character.GuildId);
+            return View(character);
+        }
+
+        // GET: Characters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var character = await _context.Characters
+                .Include(c => c.Guild)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (character == null)
+            {
+                return NotFound();
+            }
+
+            return View(character);
+        }
+
+        // POST: Characters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var character = await _context.Characters.FindAsync(id);
+            if (character != null)
+            {
+                _context.Characters.Remove(character);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CharacterExists(int id)
+        {
+            return _context.Characters.Any(e => e.Id == id);
+        }
+    }
+}
